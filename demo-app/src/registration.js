a87ea1f709b846b30744f2fb2f44ee17c204fa37
rajdhage2104 <144928213+rajdhage2104@users.noreply.github.com>
2024-11-19 15:11:43 +0530

    Update registration.js

--- demo-app/src/registration.js
+++ demo-app/src/registration.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import { makeStyles } from '@mui/material';
+import { Button } from '@mui/material';
+import { TextField } from '@mui/material';
+import { Grid } from '@mui/material';
 import { useNavigate } from "react-router-dom";
 const useStyles = makeStyles((theme) => ({
   card: {
